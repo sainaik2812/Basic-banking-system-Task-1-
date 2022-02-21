7a8d28b914089e5bfe8b840bfaad1ba4640005f2
sainaik2812 <72564107+sainaik2812@users.noreply.github.com>
2022-02-21 15:22:41 +0530

    Add files via upload

---
+++ BasicBankingSystem/index.php
@@ -0,0 +1,34 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Banking System</title>
+    <style>
+      .navbar-dark{
+        background : #83fcfce8;
+      }
+    </style>
+
+  </head>
+
+  <body>
+  <header>
+  <?php
+  include 'navbar.php';
+  ?></header>
+<main>
+  <style>
+  body{
+    background-image: url('img/bank5.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 100vh;
+ }
+</style>
+  </body>
+</html>
---
+++ BasicBankingSystem/navbar.php
@@ -0,0 +1,19 @@
+
+      <nav class="navbar navbar-expand-md navbar-dark">
+      <a class="navbar-brand" href="index.php" style="color : #1f2c9c; font-family: Copperplate,Copperplate Gothic Light,fantasy; hover "><b>____________________________________________THE UNIVERSAL BANK__________________________________ </b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #1f2c9c; font-family: Cambria,Georgia,serif; "><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color :#1f2c9c; font-family: Cambria,Georgia,serif; "><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #1f2c9c; font-family: Cambria,Georgia,serif; "><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
